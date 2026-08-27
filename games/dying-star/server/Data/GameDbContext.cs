d534dd4e3927642366c2bef6c87132a1fbe54540
vesaone <codypeisley95@hotmail.com>
2026-08-28 02:32:43 +1000

    Add Dying Star PostgreSQL game context

---
+++ games/dying-star/server/Data/GameDbContext.cs
@@ -0,0 +1,56 @@
+using DyingStar.Server.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DyingStar.Server.Data;
+
+public sealed class GameDbContext(DbContextOptions<GameDbContext> options) : DbContext(options)
+{
+    public DbSet<PlayerEntity> Players => Set<PlayerEntity>();
+    public DbSet<ArkEntity> Arks => Set<ArkEntity>();
+    public DbSet<BuildingEntity> Buildings => Set<BuildingEntity>();
+    public DbSet<ResourceLedgerEntity> ResourceLedger => Set<ResourceLedgerEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PlayerEntity>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => x.GuestTokenHash).IsUnique();
+            entity.Property(x => x.GuestTokenHash).HasMaxLength(128);
+            entity.HasOne(x => x.Ark)
+                .WithOne(x => x.Player)
+                .HasForeignKey<ArkEntity>(x => x.PlayerId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<ArkEntity>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.NexusLevel).HasDefaultValue(1);
+        });
+
+        modelBuilder.Entity<BuildingEntity>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasIndex(x => new { x.ArkId, x.Type }).IsUnique();
+            entity.Property(x => x.Type).HasConversion<string>().HasMaxLength(48);
+            entity.HasOne(x => x.Ark)
+                .WithMany(x => x.Buildings)
+                .HasForeignKey(x => x.ArkId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<ResourceLedgerEntity>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.ResourceType).HasConversion<string>().HasMaxLength(32);
+            entity.Property(x => x.Reason).HasMaxLength(128);
+            entity.Property(x => x.ActionId).HasMaxLength(128);
+            entity.HasIndex(x => x.ActionId).IsUnique();
+            entity.HasOne(x => x.Ark)
+                .WithMany(x => x.ResourceLedger)
+                .HasForeignKey(x => x.ArkId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}
