8c9fbc8deb7e27230af443a747b8ebcc779789cc
vesaone <codypeisley95@hotmail.com>
2026-08-28 02:46:58 +1000

    Add Unity economy API models

--- games/dying-star/client/Assets/Scripts/Networking/ApiModels.cs
+++ games/dying-star/client/Assets/Scripts/Networking/ApiModels.cs
@@ -20,7 +20,20 @@ namespace DyingStar.Client.Networking
         public long data;
         public long nova;
         public string serverTimeUtc;
+        public EconomySnapshot economy;
         public BuildingSnapshot[] buildings;
+    }
+
+    [Serializable]
+    public sealed class EconomySnapshot
+    {
+        public long alloyStored;
+        public long helium3Stored;
+        public double alloyPerHour;
+        public double helium3PerHour;
+        public long alloyStorageCapacity;
+        public long helium3StorageCapacity;
+        public string lastSettledAtUtc;
     }
 
     [Serializable]
@@ -46,6 +59,20 @@ namespace DyingStar.Client.Networking
         public int targetLevel;
         public string upgradeCompletesAtUtc;
         public long alloySpent;
+    }
+
+    [Serializable]
+    public sealed class CollectProductionRequest
+    {
+        public string actionId;
+    }
+
+    [Serializable]
+    public sealed class CollectProductionResponse
+    {
+        public ArkSnapshot ark;
+        public long alloyCollected;
+        public long helium3Collected;
     }
 
     [Serializable]
